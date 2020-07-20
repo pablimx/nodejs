ebb9238f21c038f05eb910582704be1e0ba6b0d8
Pablo Resende <pablim@live.co.uk>
2020-07-20 20:38:18 -0300

    Criando model de produto

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
 //Iniciando o App
 const app = express();
@@ -8,10 +9,19 @@ const app = express();
 mongoose.connect("mongodb://localhost:27017/nodeapi", {
   useNewUrlParser: true,
 });
+requireDir("./src/models");
+
+const Product = mongoose.model("Product");
 
 //Primeira Rota
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  Product.create({
+    title: "React Native",
+    description: "Build native apps with React",
+    url: "https://github.com/facebook/react-native",
+  });
+
+  return res.send("Hello World");
 });
 
 app.listen(3001);
