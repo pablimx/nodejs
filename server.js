8d691f591e1746137a6a19bf081e79c1952e3cd4
Pablo Resende <pablim@live.co.uk>
2020-07-20 20:23:37 -0300

    Conectando banco de dados

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect("mongodb://localhost:27017/nodeapi", {
+  useNewUrlParser: true,
+});
+
+//Primeira Rota
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
