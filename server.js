5b0ffe3c0a9fd0c0dcc132b1567eb3b9dc7e0979
Pablo Resende <pablim@live.co.uk>
2020-07-20 21:10:13 -0300

    Reestruturando arquivos

--- server.js
+++ server.js
@@ -11,17 +11,7 @@ mongoose.connect("mongodb://localhost:27017/nodeapi", {
 });
 requireDir("./src/models");
 
-const Product = mongoose.model("Product");
-
-//Primeira Rota
-app.get("/", (req, res) => {
-  Product.create({
-    title: "React Native",
-    description: "Build native apps with React",
-    url: "https://github.com/facebook/react-native",
-  });
-
-  return res.send("Hello World");
-});
+//Rotas
+app.use("/api", require("./src/routes"));
 
 app.listen(3001);
